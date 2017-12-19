bf0171baf9c2174faa5a29ce2ffe2cc9d27427e4
Satoshi Egi <egi@egison.org>
2017-12-19 12:19:46 +0900

    update

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(name='egison_kernel',
+      version='0.0.1',
+      description='Egison Kernel for Jupyter Notebook',
+      install_requires = [
+            'ipykernel',
+            'pexpect',
+            ],
+      author='Satoshi Egi',
+      author_email='egi@egison.org',
+      url='https://github.com/egison/egison_kernel',
+      packages=find_packages(),
+      entry_points="""
+      [console_scripts]
+      """,)
